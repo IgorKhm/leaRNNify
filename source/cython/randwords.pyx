31c70a05b8dcf090b24d837690bc81c2b7c16391
IgorKhm <igor.khme@gmail.com>
2020-09-21 16:30:23 +0200

    trying cython

--- source/cython/randwords.pyx
+++ source/cython/randwords.pyx
@@ -2,13 +2,13 @@ import cython
 from libc.stdlib cimport rand, RAND_MAX
 
 @cython.boundscheck(False)
-
+#
 def random_words(int num_of_words,tuple alphabet, double one_div_p=100.0):
     words = []
     cdef int nums_of_letters = len(alphabet)
     # cdef int one_div_p = int(1/p)
     print("rand()")
-    print(rand()/(RAND_MAX/one_div_p))
+    print((RAND_MAX/one_div_p))
     cdef int letter
     cdef int a
     for i in range(num_of_words):
@@ -18,8 +18,46 @@ def random_words(int num_of_words,tuple alphabet, double one_div_p=100.0):
         while a != 0:
             a = int(rand()/(RAND_MAX/one_div_p))
             # letter = np.random.randint(0, nums_of_letters)
-            letter =  int(rand()/(RAND_MAX/nums_of_letters))
+            letter =  int(rand()/(RAND_MAX/nums_of_letters +1))
+            # if letter >= nums_of_letters:
+            #   print("problem")
+            #   print(letter)
+            #   print(len(alphabet))
+            #   print(nums_of_letters)
             word.append(alphabet[letter])
 
         words.append(tuple(word))
     return words
+
+def is_words_in_dfa(dfa, words):
+    lenwords = len(words)
+    outputs = []
+    final_states = dfa.final_states
+    init_state = dfa.init_state
+    transitions = dfa.transitions
+    for i in range(lenwords):
+        state = init_state
+        word = words[i]
+        lenword= len(word)
+        for j in range(lenword):
+            state = transitions[state][word[j]]
+
+        outputs.append(state in final_states)
+
+    return outputs
+
+def compare_list_of_bool(lang1, lang2,num_of_samples):
+        # lenwords = len(lang1)
+        cdef int count = 0
+        for i in range(num_of_samples):
+            count += (lang1[i]!=lang2[i])
+
+
+        return (count/num_of_samples)
+
+
+    # ([(in_langs_lists[lang1])[i] == (in_langs_lists[lang2])[i] for i in
+    #                                         range(len(samples))].count(False)) / num_of_samples    # state = self.init_state
+    # #         for letter in word:
+    #             state = self.transitions[state][letter]
+    #         return state in self.final_states
--- source/random_words.py
+++ source/random_words.py
@@ -5,6 +5,7 @@ import torch
 
 from dfa import DFA
 from modelPadding import LSTM, RNNLanguageClasifier
+from randwords import random_words, is_words_in_dfa,compare_list_of_bool
 
 
 # def random_word_func_entropy_dfa(dfa:DFA,p=0.01):
@@ -45,6 +46,71 @@ def confidence_interval(language1, language2, sampler, delta=0.001, epsilon=0.00
             mistakes = mistakes + 1
             # print(mistakes)
     return mistakes / n, samples
+
+
+def confidence_interval_many_cython(languages, confidence=0.001, width=0.005, samples=None):
+    """
+    Produce the probabilistic distance of the given languages. Using the Chernoff-Hoeffding bound we get that
+    in order to have:
+        P(S - E[S]>width)< confidence
+        S = 1/n(n empirical examples)
+
+    the number of examples that one needs to use is:
+        #examples = log(2 / confidence) / (2 * width * width)
+
+    For more details:
+    https://en.wikipedia.org/wiki/Hoeffding%27s_inequality
+
+    """
+    num_of_lan = len(languages)
+    if num_of_lan < 2:
+        raise Exception("Need at least 2 languages to compare")
+
+    num_of_samples = np.log(2 / confidence) / (2 * width * width)
+    print("size of sample:" + str(int(num_of_samples)))
+    if samples is None:
+        samples = random_words(num_of_samples,languages[0].alphabet)
+        # samples = [sampler(languages[0].alphabet) for _ in range(int(num_of_samples))]
+
+    in_langs_lists = []
+    i = 0
+    sys.stdout.write('\r Creating bool lists for each lan:  {}/{} done'.format(i, num_of_lan))
+    torch.cuda.empty_cache()
+    for lang in languages:
+        if not isinstance(lang, RNNLanguageClasifier):
+            in_langs_lists.append(is_words_in_dfa(lang,samples))
+            # in_langs_lists.append([lang.is_word_in(w) for w in samples])
+        else:
+            rnn_bool_list = []
+            batch_size = 1000
+            num_of_batches = int((len(samples) / batch_size))
+            for i in range(num_of_batches):
+                if i % 50 == 0:
+                    print("done {}/{}".format(i, num_of_batches))
+                batch = samples[i * batch_size:(i + 1) * batch_size]
+                batch_out = (lang.is_words_in_batch(batch) > 0.5)
+                rnn_bool_list.extend(batch_out)
+            batch = samples[num_of_batches * batch_size:len(samples)]
+            batch_out = (lang.is_words_in_batch(batch) > 0.5)
+            rnn_bool_list.extend(batch_out)
+            in_langs_lists.append(rnn_bool_list)
+
+    output = []
+    for i in range(num_of_lan):
+        output.append([1] * num_of_lan)
+
+    for lang1 in range(num_of_lan):
+        for lang2 in range(num_of_lan):
+            if lang1 == lang2:
+                output[lang1][lang2] = 0
+            elif output[lang1][lang2] == 1:
+                output[lang1][lang2] = compare_list_of_bool(in_langs_lists[lang1],in_langs_lists[lang2],num_of_samples)
+                # output[lang1][lang2] = ([(in_langs_lists[lang1])[i] == (in_langs_lists[lang2])[i] for i in
+                #                          range(len(samples))].count(False)) / num_of_samples
+
+    print("done with confidence interval")
+    return output, samples
+
 
 
 def confidence_interval_many(languages, sampler, confidence=0.001, width=0.005, samples=None):
